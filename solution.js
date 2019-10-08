1a1bc3a540ead86570e964938fb3e3ed766b5e0d
PeglegPierre <53906424+PeglegPierre@users.noreply.github.com>
2019-10-08 07:45:51 +0200

    Add files via upload

---
+++ solution.js
@@ -0,0 +1,27 @@
+//isEven
+function isEven (num){
+	return num % 2 ===0;
+}
+
+//factorial
+function factorial(num){
+
+	var result = 1;
+
+	for(var i = 2; i <= num; i++){
+		result *= i;
+	}
+	return result;
+}
+
+function kebabToSnake(str) {
+
+	var newStr = str.replace(/-/g , "_");
+
+	return newStr;
+
+
+}
+
+
+
